56f796e23a0d4d1cafd181ea8b35485a3aa7993e
Atom Alexander M. Nava <atommunoznava@gmail.com>
2024-08-02 17:22:39 -0600

    Update Store.java

--- Store.java
+++ Store.java
@@ -5,6 +5,7 @@ import dev.lpa.burguer.Meal;
 public class Store {
     public static void main(String[] args) {
         Meal regularMeal = new Meal();
+        regularMeal.addToppings("Ketchup", "Mayo", "Bacon", "Cheddar");
         System.out.println(regularMeal);
 
         Meal NYRegularMeal = new Meal(0.79);
